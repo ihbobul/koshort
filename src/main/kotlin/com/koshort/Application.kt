97ba03f35c2bcabbd62bdcf9fbaaacad4c86d947
Ihor Bobulyć <ihor.bobulych@bluelions.ee>
2024-12-13 21:47:22 +0200

    chore: initial project setup

---
+++ src/main/kotlin/com/koshort/Application.kt
@@ -0,0 +1,17 @@
+package com.koshort
+
+import com.koshort.plugins.configureRouting
+import com.koshort.plugins.configureSerialization
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
